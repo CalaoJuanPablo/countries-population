2564ac69d1f3af29da4e77639fd81ea385aad8f1
Juan Pablo Calao <jcalaop@gmail.com>
2020-10-28 00:06:02 -0500

    add search bar to country list

--- src/components/CountriesList/index.js
+++ src/components/CountriesList/index.js
@@ -7,6 +7,12 @@ export function CountriesList() {
   const [countries, setCountries] = React.useState([])
   const [isLoading, setIsLoading] = React.useState(false)
   const [isError, setIsError] = React.useState(false)
+  const [search, setSearch] = React.useState('')
+
+  const filterCountries = (countries, filterCriteria) =>
+    countries.filter(country =>
+      country.name.toLowerCase().includes(filterCriteria.toLowerCase())
+    )
 
   React.useEffect(function () {
     function getCountries() {
@@ -27,17 +33,35 @@ export function CountriesList() {
     getCountries()
   }, [])
 
+  const countriesToRender = React.useMemo(
+    () => filterCountries(countries, search),
+    [countries, search]
+  )
+
   return (
-    <div className={styles.CountriesList}>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : isError ? (
-        <h1>Error</h1>
-      ) : (
-        countries.map((country, idx) => (
-          <CountryCard key={`country-${idx.toString()}`} {...country} />
-        ))
-      )}
-    </div>
+    <>
+      <form
+        className={styles.searchBarForm}
+        onSubmit={ev => ev.preventDefault()}
+      >
+        <h1>Search for a country</h1>
+        <input
+          name='search-bar'
+          value={search}
+          onChange={ev => setSearch(ev.target.value)}
+        />
+      </form>
+      <div className={styles.CountriesList}>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : isError ? (
+          <h1>Error</h1>
+        ) : (
+          countriesToRender.map((country, idx) => (
+            <CountryCard key={`country-${idx.toString()}`} {...country} />
+          ))
+        )}
+      </div>
+    </>
   )
 }
